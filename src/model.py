135b2e1ce15b0b0509329c5018e3cf2dbb44977b
by256 <by256@cam.ac.uk>
2022-05-24 11:24:59 +0100

    change warmup_epochs to 0.1*max_epochs

--- src/model.py
+++ src/model.py
@@ -71,7 +71,8 @@ class LightningModel(pl.LightningModule):
                                      lr=self.hparams.lr,
                                      weight_decay=self.hparams.weight_decay)
         lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer,
-                                                     warmup_epochs=10,
+                                                     warmup_epochs=int(
+                                                         0.1*self.trainer.max_epochs),
                                                      max_epochs=self.trainer.max_epochs)
         return {'optimizer': optimizer,
                 'lr_scheduler': {'scheduler': lr_scheduler}}
