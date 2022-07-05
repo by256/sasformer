fbaa7d7395f9472edc22a54ef69b049407c172f0
by256 <by256@cam.ac.uk>
2022-07-05 15:29:26 +0100

    change warmup_epochs to 5% of max_epochs

--- src/model.py
+++ src/model.py
@@ -124,7 +124,8 @@ class SASPerceiverIOModel(pl.LightningModule):
                                      lr=self.hparams.lr,
                                      weight_decay=self.hparams.weight_decay)
         lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer,
-                                                     warmup_epochs=15,
+                                                     warmup_epochs=int(
+                                                         0.05*self.trainer.max_epochs),
                                                      max_epochs=self.trainer.max_epochs)
         return {'optimizer': optimizer,
                 'lr_scheduler': {'scheduler': lr_scheduler}}
