9bffa9158a6f715942c559f21f63036314fdf891
by256 <by256@cam.ac.uk>
2024-01-28 21:01:09 +0000

    add training and results scripts

---
+++ scripts/results.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+data_dir="../data/sas-55m-20k"
+
+if [ ! -f "$data_dir/scales.json" ]; then
+    # If scales.json does not exist, move it from sasformer/data to data_dir
+    cp ../data/scales.json "$data_dir"
+fi
+
+mkdir -p results
+
+python ../sasformer/results.py --ckpt_path ../checkpoints/final.ckpt --data_dir ../data/sas-55m-20k/
